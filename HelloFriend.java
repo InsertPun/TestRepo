9e0ece48dbe54234ca1c20a43a3a00f376c22b88
InsertPun <stanleyhuang905@gmail.com>
2025-09-10 19:29:39 -0400

    Updated message

--- HelloFriend.java
+++ HelloFriend.java
@@ -1,5 +1,5 @@
 public class HelloFriend {
     public static void main(String[] args) {
-        System.out.println("Hello Friend!");
+        System.out.println("Hello Friend! How are you?");
     }
 }
