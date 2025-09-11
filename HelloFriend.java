b29c163db44c25798555db8b7e778d8e20f28311
PompaTy <tyler.pompa@gmail.com>
2025-09-11 09:52:27 -0400

    added line

--- HelloFriend.java
+++ HelloFriend.java
@@ -1,5 +1,6 @@
 public class HelloFriend {
    public static void main(String[] args) {
       System.out.println("Hello Friend! How are you?");
+      System.out.println("Im good! How are you?");
    }
 }
